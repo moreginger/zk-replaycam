96b90afcfc1a63d2b2f3ff03289b4d24ab606752
Tim Moore <moreginger@warpmail.net>
2023-03-21 22:33:41 +0000

    Extract events from log.

--- scripts/generate-stats.py
+++ scripts/generate-stats.py
@@ -3,16 +3,24 @@
 import json
 import re
 
+re_event = 'event, (?P<type>.+?),.+$'
 re_merge = 'merging events, (?P<type>.+?)$'
 re_mie = 'mie, (?P<type>.+?),.+$'
 
 data = {
+    'event': {},
     'merge': {},
     'mie': {}
 }
 
 with open('infolog.txt') as file:
     while (line := file.readline()):
+        match = re.search(re_event, line)
+        if match:
+            event_type = match.group('type')
+            data['event'].setdefault(event_type, 0)
+            data['event'][event_type] += 1
+            continue
         match = re.search(re_merge, line)
         if match:
             event_type = match.group('type')
@@ -24,5 +32,6 @@ with open('infolog.txt') as file:
             event_type = match.group('type')
             data['mie'].setdefault(event_type, 0)
             data['mie'][event_type] += 1
+            continue
 
 print(json.dumps(data))
